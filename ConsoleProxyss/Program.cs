0e28d9dbe8e00b1e6b2a0a1fdcf7edc56328fd9d
Евгений Курилин <spandae1404@gmail.com>
2021-08-24 01:18:09 +0300

    refactoring/changed names

--- ConsoleProxyss/Program.cs
+++ ConsoleProxyss/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
@@ -9,37 +10,43 @@ namespace ProxyChekers
 { //https://free-proxy-list.net/
     internal class Program
     {
-        //статистическое поле сохраняющее дальше в коде последнее значение.
-        public static int mincoun = 0;
+        
+        /// <summary>
+        /// статистическое поле сохраняющее дальше в коде последнее значение.
+        /// </summary>
+        private static int keepValues = 0;
 
-        // Метод для чека прокси. min & max диапозон для проксей
-        public static void Cheker(string[] ms, int min, int max)
+
+        /// <param Cheker = "Cheker" > Метод для чека прокси. min & max диапозон для проксей</param >
+        public static void Cheker(string[] ip, int min, int max)
         {
-            //оборочивание в цикл for для отлова исключение и перехождения к next proxy
+            /// оборочивание в цикл for для отлова исключение и перехождения к next proxy
             for (var j = min; j <= max; j++)
             {
-                //переменная для блока catch. потому что i цикла for из блока try недоступна
-                var cv = 0;
+                /// <remarks>
+                /// переменная для блока catch. потому что i цикла for из блока try недоступна
+                /// </remarks > 
+                var empty = 0;
                 try
                 {
                     for (var i = min; i <= max; i++)
                     {
-                        //чек прокси
-                        cv = i;
-                        var proxy = new WebProxy(ms[i], true);
-                        var req = WebRequest.CreateHttp("https://www.google.com/");
-                        req.Proxy = proxy;
-                        req.Timeout = 2000;
-                        var Response = (HttpWebResponse)req.GetResponse();
-                        var RespString = new StreamReader(Response.GetResponseStream()).ReadToEnd();
-                        Console.WriteLine(ms[cv] + "живой");
+                        /// чек прокси
+                        empty = i;
+                        var proxy = new WebProxy(ip[i], true);
+                        var webRequest = WebRequest.CreateHttp("https://www.google.com/");
+                        webRequest.Proxy = proxy;
+                        webRequest.Timeout = 2000;
+                        var response = (HttpWebResponse)webRequest.GetResponse();
+                        var respString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                        Console.WriteLine(ip[empty] + "живой");
                     }
 
                 }
-                //исключения
+                /// исключения
                 catch (WebException)
                 {
-                    Console.WriteLine(ms[cv] + "мертвец");
+                    Console.WriteLine(ip[empty] + "мертвый");
 
                 }
             }
@@ -47,64 +54,79 @@ namespace ProxyChekers
 
         private static void Main()
         {
-            var list = new List<string>();
+            var carryover = new List<string>();
 
-            int min;
-            int max;
-            // кол-во прокси на один поток
-            int proxyon1thread;
-            // кол-во потоков
-            var nathreads = 10;
-            //для foreach
+            int min, max;
+            /// <remarks>
+            /// кол-во прокси на поток
+            /// </remarks> 
+            int countProxyForOneThread;
+            /// <remarks>
+            /// кол-во потоков
+            /// </remarks> 
+            var threads = 7;
+            /// <remarks>
+            /// для цикла foreach
+            /// </remarks> 
             var chekadd = false;
             string writetoprox = null;
-            //берем прокся
+            /// <remarks>
+            /// поднятия прокси апишников из файла
+            /// </remarks> 
             var file = File.ReadAllText(@"E:\projects\proxy.txt");
 
-            //считывание прокси без пробелов и тд в листе
-            foreach (var r in file)
-                if ((r != ' ') & (r != '\r') & (r != '\n'))
+            /// считывание прокси без пробелов и тд в листе
+            foreach (var symbols in file)
+                if ((symbols != ' ') && (symbols != '\r') && (symbols != '\n'))
                 {
-                    writetoprox += r;
+                    writetoprox += symbols;
                     chekadd = false;
                 }
                 else
                 {
-                    if (chekadd == false) list.Add(writetoprox);
+                    if (chekadd == false) carryover.Add(writetoprox);
                     chekadd = true;
                     writetoprox = null;
                 }
-            // Делаю переменную ссылки на массив типа string, а затем присваиваю ей коллекцию, которую с помощью метода ToArray я перевёл в массив.
-            var ms = list.ToArray();
-            proxyon1thread = ms.Length / nathreads;
-            // массив  потоков
-            var th = new Thread[10];
-            // присвоение значения пременным
+            /// <remarks>
+            /// Делаю переменную ссылки на массив типа string, а затем присваиваю ей коллекцию, которую с помощью метода ToArray я перевёл в массив.
+            /// </remarks >  
+            var _link = carryover.ToArray();
+            countProxyForOneThread = _link.Length / threads;
+
+            /// <remarks>
+            /// Массив потоков
+            /// </remarks > 
+            var threads1 = new Thread[7];
+
+            /// /// <remarks>
+            /// Присвоение значение переменным 
+            /// </remarks > 
             min = 0;
             max = 0;
-            int mt;
-            //в этом цикле for я добавляю в массив потоков лямбда-выражение, которое вмещает в себя метод Checker
-            for (var i = 1; i <= nathreads; i++)
+            int range;
+            /// в этом цикле for я добавляю в массив потоков лямбда-выражение, которое вмещает в себя метод Checker
+            for (var i = 1; i <= threads; i++)
             {
-                //конечное значение для проверки
-                mt = proxyon1thread * i;
-                //диапозон проверки прокси от 0 и до proxyon1thread
+                /// конечное значение для проверки
+                range = countProxyForOneThread * i;
+                /// диапозон проверки прокси от 0 и до countProxyForOneThread
                 if (i == 0)
                 {
-                    th[i - 1] = new Thread(() => Cheker(ms, 0, proxyon1thread));
-                    //запоминаем индекс проверенного прокси
-                    mincoun = proxyon1thread;
+                    threads1[i - 1] = new Thread(() => Cheker(_link, 0, countProxyForOneThread));
+                    /// запоминаем индекс проверенного прокси
+                    keepValues = countProxyForOneThread;
                 }
                 else
                 {
-                    //добавляем поток, где от значений переменной mincoun до значения переменной mt ( mt отображет тот диапазон, до которого метод должен проверить ) метод проверяет прокси.
-                    th[i - 1] = new Thread(() => Cheker(ms, mincoun, mt));
-                    // запоминаем значение которое было конечным. до которого мы проверяли
-                    mincoun = mt;
+                    /// добавляем поток, где от значений переменной _link до значения переменной mt ( mt отображет тот диапазон, до которого метод должен проверить ) метод проверяет прокси.
+                    threads1[i - 1] = new Thread(() => Cheker(_link, keepValues, range));
+                    /// запоминаем значение которое было конечным. до которого мы проверяли
+                    keepValues = range;
                 }
             }
-            //запуск потоков в цикле
-            for (var i = 0; i < th.Length; i++) th[i].Start();
+            /// запуск потоков в цикле
+            for (var i = 0; i < threads1.Length; i++) threads1[i].Start();
         }
         
     }
